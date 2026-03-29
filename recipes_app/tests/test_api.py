ab0376e94dec9992716c22e09262addff2ff38a8
Younes Darabi <younes.darabi@outlook.com>
2026-03-29 15:47:29 +0200

    update test happy & Unhappy

--- recipes_app/tests/test_api.py
+++ recipes_app/tests/test_api.py
@@ -1,12 +1,14 @@
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
-from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 
+from recipes_app.models import Recipe
+ 
 
-
-class RecipeAPITestCase(APITestCase):
+class RecipeAPITestCaseHappy(APITestCase):
 
     def setUp(self):
         self.user = User.objects.create_user(
@@ -16,6 +18,11 @@ class RecipeAPITestCase(APITestCase):
         self.token = Token.objects.create(user=self.user)
         self.client = APIClient()
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        self.recipe_data = {
+            "title": "Title test 2",
+            "description": "Description test 2",
+            "author": self.user.id
+        }
 
     def test_get_recipe(self):
         url = reverse('recipe-list')
@@ -24,10 +31,51 @@ class RecipeAPITestCase(APITestCase):
 
     def test_post_recipe(self):
         url = reverse('recipe-list')
-        data = {
+        response = self.client.post(url, self.recipe_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_recipe_detail(self):
+        recipe = Recipe.objects.create(
+        title="Test",
+        description="Test desc",
+        author=self.user
+        )
+        url = reverse('recipe-detail', kwargs={'pk': recipe.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class RecipeAPITestCaseUnhappy(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpass'
+        )
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.recipe_data = {
             "title": "Title test 2",
             "description": "Description test 2",
             "author": self.user.id
         }
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_get_recipe(self):
+        url = reverse('recipe-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_post_recipe(self):
+        url = reverse('recipe-list')
+        response = self.client.post(url, self.recipe_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_recipe_detail(self):
+        recipe = Recipe.objects.create(
+        title="Test",
+        description="Test desc",
+        author=self.user
+        )
+        url = reverse('recipe-detail', kwargs={'pk': recipe.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
